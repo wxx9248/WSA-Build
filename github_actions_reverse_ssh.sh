dfe5ae80b5172f5fccbcefcaaf6390851496c5e3
Jake Wang <wxx9248@qq.com>
2023-05-29 23:50:19 +0800

    Update script (#45)

--- github_actions_reverse_ssh.sh
+++ github_actions_reverse_ssh.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sudo apt install -y socat
+if [[ "$REVERSE_SSH" != "true" ]]; then
+    echo "REVERSE_SSH is not true, skipping"
+    exit 0
+fi
+
+apt-get install -y socat
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo "$REVERSE_SSH_PUBLIC_KEY" >~/.ssh/authorized_keys
 echo Current user: $(whoami)
-socat tcp4-connect:$REVERSE_SSH_REMOTE_ADDRESS_PORT tcp4-connect:127.0.0.1:22 &
+socat tcp4-connect:$REVERSE_SSH_REMOTE_ADDRESS_PORT tcp4-connect:127.0.0.1:22
