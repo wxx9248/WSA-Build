97a8d7dba12c0948b418c66dc3ee1c30e1dbb95a
wxx9248 <wxx9248@wxx9248.top>
2024-10-14 02:07:23 -0700

    Fix Broken Actions Workflow (#65)

--- build_wsa.sh
+++ build_wsa.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-
-set -e
+set -e -x
 
 declare -A RELEASE_TYPE_MAP=(
-    ["retail"]="retail"
-    ["release preview"]="RP"
-    ["insider slow"]="WIS"
-    ["insider fast"]="WIF"
+    ["Retail"]="retail"
+    ["Release Preview"]="RP"
+    ["Insider Slow"]="WIS"
+    ["Insider Fast"]="WIF"
 )
 
 if [[ "$WSA_BUILD_REMOVE_AMAZON" == "true" ]]; then
     WSA_BUILD_REMOVE_AMAZON_FLAG='--remove-amazon'
 fi
 
+if [[ "$WSA_BUILD_INSTALL_GAPPS" == "true" ]]; then
+    WSA_BUILD_INSTALL_GAPPS_FLAG='--install-gapps'
+fi
+
 cd MagiskOnWSALocal/scripts
-bash -x ./build.sh \
+bash -e -x ./build.sh \
     --arch "$WSA_BUILD_ARCHITECTURE" \
     --release-type "${RELEASE_TYPE_MAP[$WSA_BUILD_RELEASE_TYPE]}" \
     --magisk-ver "$WSA_BUILD_MAGISK_VERSION" \
-    --gapps-brand "$WSA_BUILD_GOOGLE_APPS_BRAND" \
-    --gapps-variant "$WSA_BUILD_GOOGLE_APPS_VARIANT" \
+    "$WSA_BUILD_INSTALL_GAPPS_FLAG" \
     "$WSA_BUILD_REMOVE_AMAZON_FLAG" \
     --root-sol "$WSA_BUILD_ROOT_SOLUTION" 2>&1
 cd ../..
